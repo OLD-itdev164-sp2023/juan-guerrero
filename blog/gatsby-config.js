fb21e454f753dd8ce3884bcbab41daaef4a32cd4
Juan Guerrero <guerrjm1@gmatc.matc.edu>
2023-02-12 15:16:48 -0600

    Completed Activity 3

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -43,9 +43,7 @@ module.exports = {
     }
 },
 
-    `gatsby-transformer-remark`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+   
     
     {
         resolve: `gatsby-plugin-manifest`,
