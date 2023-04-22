9cce33e8a236c17bcd85b10c3f4497b1e701408e
Juan Guerrero <guerrjm1@gmatc.matc.edu>
2023-04-22 18:40:40 -0500

    Completed Activity 9

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -10,10 +10,11 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import styled, {ThemeProvider} from 'styled-components'
 
-
 import { Gray } from './themes/Gray'
-import {Header} from "./Header"
+import { Header } from "./Header"
 import "./layout.css"
+import { Main } from './Main'
+import { Footer } from './Footer'
 
 const Content = styled.div`
   margin: 0 auto;
@@ -36,8 +37,8 @@ const Layout = ({ children }) => {
     <ThemeProvider theme={Gray}>
       <Header siteTitle={data.site.siteMetadata.title || `Title`} />
       <Content>
-        <main>{children}</main>
-        <footer
+        <Main m={20}>{children}</Main>
+        <Footer
           style={{
             marginTop: `var(--space-5)`,
             fontSize: `var(--font-sm)`,
@@ -46,7 +47,7 @@ const Layout = ({ children }) => {
           © {new Date().getFullYear()} &middot; Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        </Footer>
       </Content>
     </ThemeProvider>
   )
